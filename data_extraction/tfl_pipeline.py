eadf2f1778e99836e8906ca030b432d0624b8498
neil pandit <kshitijneilpandit@gmail.com>
2026-06-24 05:21:20 +0100

    feat(tfl pipeline): adds unfinished fetcher algorithm for tfl data

--- data_extraction/tfl_pipeline.py
+++ data_extraction/tfl_pipeline.py
@@ -1,3 +1,14 @@
 from config import TFL_API_KEY
 import pandas as pd
 import requests
+
+BASE_TFL_URL = "https://api.tfl.gov.uk"
+
+# fetcher:
+
+def fetch_tfl_data(end_point, extra_params=None):
+    url = f"{BASE_TFL_URL}/{end_point}"
+    params = {"app_key" : TFL_API_KEY}
+
+    if extra_params is not None:
+        params.update(extra_params)
