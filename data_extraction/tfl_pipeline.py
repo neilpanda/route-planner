12ef9988a31c956ed7f118d8621d5e96d916198f
Pandit <np1024@ic.ac.uk>
2026-06-25 22:07:46 +0100

    fix(tfl pipeline): completes fetcher algorithm
    adds docstrings, exceptions, and outputs the url response as a .json file

--- data_extraction/tfl_pipeline.py
+++ data_extraction/tfl_pipeline.py
@@ -7,8 +7,21 @@ BASE_TFL_URL = "https://api.tfl.gov.uk"
 # fetcher:
 
 def fetch_tfl_data(end_point, extra_params=None):
+    """Fetches a .json file containing API data for a given endpoint."""
     url = f"{BASE_TFL_URL}/{end_point}"
     params = {"app_key" : TFL_API_KEY}
 
-    if extra_params is not None:
+    if extra_params: # uses this version to detect empty structures ie. {}
         params.update(extra_params)
+    
+    print(f"Fetching at {url}...")
+    print()
+    response = requests.get(url, params=params)
+
+    try:
+        response.raise_for_status()
+        print("Fetch success!")
+        return response.json()
+    except requests.exceptions.HTTPError as err:
+        print(f"API error: {err}")
+        return None
