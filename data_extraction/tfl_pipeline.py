e5859781d8707e553d98b8d6c002c62c77b8dccd
neil pandit <kshitijneilpandit@gmail.com>
2026-06-30 20:31:16 +0100

    docs(tfl pipeline): adds a docstring for fetch_tfl_data

--- data_extraction/tfl_pipeline.py
+++ data_extraction/tfl_pipeline.py
@@ -1,4 +1,5 @@
 from config import TFL_API_KEY
+from typing import Any
 import pandas as pd
 import requests
 
@@ -6,14 +7,27 @@ BASE_TFL_URL = "https://api.tfl.gov.uk"
 
 # fetcher:
 
-def fetch_tfl_data(end_point, extra_params=None):
-    """Fetches a .json file containing API data for a given endpoint."""
+def fetch_tfl_data(end_point: str, extra_params: dict[str, Any] | None = None):
+    """_Acquires a JSON file from TFL API in the form of a url request._
+
+    Args:
+        end_point (_str_): _end of the URL request for data specific TFL API request._
+        extra_params (_dict_, None): _contains extra parameters to include in the
+        search. Defaults to None.
+
+    Returns:
+        response (_list or dict_): _the JSON response if successful_
+
+    Raises:
+        HTTPError (_exception_): _if unsuccessful_
+    """
+
     url = f"{BASE_TFL_URL}/{end_point}"
     params = {"app_key" : TFL_API_KEY}
 
     if extra_params: # uses this version to detect empty structures ie. {}
         params.update(extra_params)
-    
+
     print(f"Fetching at {url}...")
     print()
     response = requests.get(url, params=params)
@@ -25,8 +39,8 @@ def fetch_tfl_data(end_point, extra_params=None):
     except requests.exceptions.HTTPError as err:
         print(f"API error: {err}")
         return None
-    
+
 # parsers:
 
-def parse_bikes():
+def parse_bikes(raw_json):
     pass
